2bd522ea59d04c26aade026fc7783bbb12b3acd2
Nasheverdov <nikfilk2030@gmail.com>
2025-01-02 18:07:01 +0000

    nice API version

--- init.sql
+++ init.sql
@@ -2,18 +2,9 @@ CREATE DATABASE IF NOT EXISTS telegram_bot;
 
 USE telegram_bot;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    telegram_id BIGINT UNIQUE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS user_configuration (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    telegram_id BIGINT NOT NULL,
+    chat_id BIGINT NOT NULL,
     message TEXT NOT NULL,
-    interval_minutes INT NOT NULL,
-    remaining_count INT NOT NULL,
-    last_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (telegram_id) REFERENCES users(telegram_id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
